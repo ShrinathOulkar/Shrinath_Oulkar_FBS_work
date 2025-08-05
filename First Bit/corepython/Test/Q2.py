1c6ac16a5cc6df364c59e0b97766e581a744b1ea
ShrinathOulkar <shrinathoulkar@gmail.com>
2025-08-05 16:26:24 +0530

    Delete First Bit/corepython/Test directory

--- First Bit/corepython/Test/Q2.py
+++
@@ -1,8 +0,0 @@
-
-p = int(input("Enter principal : "))
-r = float(input("Enter rate : "))
-t = int(input("Enter Time : "))
-
-si = (p * r * t)/100
-
-print(f"Simple Interest is :{ si } ")
--- First Bit/corepython/Test/Q3.py
+++
@@ -1,6 +0,0 @@
-km = int(input(" Enter Distsnce In KM :"))
-
-meter = km *1000
-centi = meter * 100
-
-print(f"Meter :{meter}\n Centimeter : {centi}")
