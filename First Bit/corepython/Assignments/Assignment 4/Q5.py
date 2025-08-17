75af2526ad45518d99e0d86527bf882757be261f
ShrinathOulkar <shrinathoulkar@gmail.com>
2025-08-17 23:05:50 +0530

    Q5.py

--- First Bit/corepython/Assignments/Assignment 4/Q5.py
+++ First Bit/corepython/Assignments/Assignment 4/Q5.py
@@ -4,9 +4,11 @@ n = int(input("Enter Number :"))
 a = 0 
 b = 1
 count = 0
+print(a , b , end=" ")
 while(count < n):
     c = a + b
     print(c,end =" ")
     a ,b = b ,c
     count = count + 1    
+
 
